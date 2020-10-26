8fa669da6387945701aad1cd6f33ac5db7125052
Caitlin Stanton <caitlinstanton44@gmail.com>
2020-10-26 02:06:37 -0400

    lab 7

---
+++ lab6/jupyter/scripts/robot_interface.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+import sys
+import rospy
+from geometry_msgs.msg import PoseStamped, Point
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+import time
+import numpy as np
+from lab6.msg import PlotPoint
+from lab6.msg import MapPoints
+from lab6.msg import SimPose
+import math
+
+ODOM = 0
+GT = 1
+BEL = 2
+MAP = 3
+
+
+class Pose():
+    def __init__(self):
+        # Odom values
+        self.x = 0
+        self.y = 0
+        self.a = 0
+
+        # Ground Truth values
+        self.gt_x = 0
+        self.gt_y = 0
+        self.gt_a = 0
+
+
+class Robot():
+    def __init__(self):
+        print ("Using python version:", sys.version)
+
+        self.pose = Pose()
+
+        self.laser_range = 0.0
+
+        self.subscriber_pose_time_delay = 0.01
+
+        # Temp variables
+        self.temp_pose = Pose()
+
+    def odom_callback(self, data):
+        self.temp_pose.x = data.odom_x
+        self.temp_pose.y = data.odom_y
+        self.temp_pose.a = data.odom_a
+        self.temp_pose.gt_x = data.gt_x
+        self.temp_pose.gt_y = data.gt_y
+        self.temp_pose.gt_a = data.gt_a
+
+        self.pose = self.temp_pose
+
+
+        # time.sleep(self.subscriber_pose_time_delay)
+
+    def laser_callback(self, data):
+        self.laser_range = data.ranges[0]
+
+
+    def set_vel(self, v, w):
+        cmd_vel = Twist()
+        cmd_vel.linear.x = v
+        cmd_vel.angular.z = w
+
+        self.vel_pub.publish(cmd_vel)
+
+    def get_pose(self):
+        return (self.pose.x, self.pose.y, self.pose.a)
+
+    def get_gt_pose(self):
+        return (self.pose.gt_x, self.pose.gt_y, self.pose.gt_a)
+
+    def get_laser_data(self):
+        return self.laser_range
+
+    def send_to_plot(self, x, y, plot_type):
+        self.traj_pub.publish(x, y, plot_type)
+
+    def initialize(self):
+        rospy.init_node('lab6_robot_controller', anonymous=True)
+        rospy.Subscriber("/sim_pose",
+                         SimPose,
+                         self.odom_callback,
+                         queue_size=1)
+
+        rospy.Subscriber("/base_scan",
+                         LaserScan,
+                         self.laser_callback,
+                         queue_size=1)
+
+        self.vel_pub = rospy.Publisher('/cmd_vel',
+                                       Twist,
+                                       queue_size=1)
+
+        self.traj_pub = rospy.Publisher('/traj_points',
+                                        PlotPoint,
+                                        queue_size=300)
+
---
+++ lab7/lab7_base_code/setup.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+set -e
+
+# Color Variables
+NOCOLOR='\033[0m'
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+BLUE='\033[1;34m'
+YELLOW='\033[1;33m'
+WHITE='\033[1;37m'
+
+# Variables
+LAB_NAME="lab7"
+
+PWD=$(pwd)
+HOME_DIR=$HOME"/"
+SOURCE_DIR=$PWD"/"
+TARGET_DIR=$HOME_DIR"catkin_ws/src/"
+CATKIN_DIR=$HOME_DIR"catkin_ws/"
+WORK_DIR=$TARGET_DIR$LAB_NAME"/"
+
+LOG_FILE=$SOURCE_DIR"output_"$LAB_NAME".log"
+
+# Project name
+ZIP_FILE=$LAB_NAME".zip"
+
+function print_if_file_exists
+{
+  if [ -f "$1" ]; then
+      echo "   $1"
+      echo "   $1" >> "$LOG_FILE"
+  fi
+}
+
+
+################## CHECK FILES ###################
+echo -e "${BLUE} > Log output written to: $LOG_FILE ${NOCOLOR}"
+echo -e "${BLUE} > Lab Work Directory: $TARGET_DIR$LAB_NAME/scripts/ ${NOCOLOR}"
+
+# Log output (INIT)
+echo " > Lab Work Directory: $TARGET_DIR" > "$LOG_FILE"
+
+echo -e "${YELLOW}Validating...${NOCOLOR}"
+# Test if in the right directory
+FOUND_ZIP=1
+if [ ! -f "$LAB_NAME.zip" ]; then
+    echo -e "   ${RED}Cannot find "$LAB_NAME".zip in the current directory.${NOCOLOR}"
+    echo "Cannot find "$LAB_NAME".zip in the current directory." >> "$LOG_FILE"
+    FOUND_ZIP=0
+fi
+
+FOUND_ALISES=1
+if [ ! -f "bash_aliases" ]; then
+    echo -e "   ${RED}Cannot find bash_aliases in the current directory.${NOCOLOR}"
+    echo "Cannot find bash_aliases in the current directory." >> "$LOG_FILE"
+    FOUND_ALISES=0
+fi
+if [[ "$FOUND_ZIP" -eq "0" || "$FOUND_ALISES" -eq "0" ]]; then
+   echo -e "${RED}Please check if you are running the setup script from within the "$LAB_NAME"_base_code directory.${NOCOLOR}"
+   echo "Exiting..."
+   exit
+fi
+
+# Check if files exist
+if [ -d "$WORK_DIR" ]; then
+    echo ""
+    echo "The $LAB_NAME directory already exists."
+    echo "All the $LAB_NAME files, including the following files will be overwritten if you continue: "
+    print_if_file_exists $WORK_DIR"scripts/robot_interface.py"
+    print_if_file_exists $WORK_DIR"scripts/$LAB_NAME.ipynb"
+    echo ""
+    while true; do
+      read -p "Are you sure you want to overwrite the files?(y/n)" yn
+      case $yn in
+          [Yy]* ) break;;
+          [Nn]* ) echo "Exiting..."; exit;;
+          * ) echo "Please answer yes or no.";;
+      esac
+    done
+fi
+###################################################
+
+
+################## EXTRACT FILES ##################
+echo -e "${YELLOW}Step 1/3: Extracting Files to: $TARGET_DIR ${NOCOLOR}"
+
+# Log output
+echo "------- STEP: 1 -------" >> "$LOG_FILE"
+
+# Unzip package into catkin workspace
+unzip -o $ZIP_FILE -d $TARGET_DIR >> "$LOG_FILE"
+###################################################
+
+
+################## SETUP ALIASES ##################
+echo -e "${YELLOW}Step 2/3: Setting up commands${NOCOLOR}"
+
+# Remove aliases file
+rm -f $HOME_DIR/.bash_aliases
+
+# Create aliases file
+cp "bash_aliases" $HOME/.bash_aliases
+
+# Add line to .bashrc to source aliases file
+grep -qxF "source ~/.bash_aliases" $HOME/.bashrc || echo -e "\nsource ~/.bash_aliases" >> $HOME/.bashrc
+
+# Log output
+echo "------- STEP: 2 -------" >> "$LOG_FILE"
+tail -5 $HOME/.bashrc >> "$LOG_FILE"
+echo "--- ls:" >> "$LOG_FILE"
+ls -ltr $HOME/.bash_aliases &>> "$LOG_FILE"
+echo "--- cat:" >> "$LOG_FILE"
+cat $HOME/.bash_aliases &>> "$LOG_FILE"
+echo "" >> "$LOG_FILE"
+###################################################
+
+################## CATKIN_MAKE ##################
+echo -e "${YELLOW}Step 3/3: Compiling Project${NOCOLOR}"
+mkdir -p "${TARGET_DIR}"
+
+cd "${CATKIN_DIR}"
+
+set +e
+# Log output
+echo "------- STEP: 3 -------" >> "$LOG_FILE"
+catkin_make &>> "$LOG_FILE"
+
+if [ $? -eq 0 ]
+then
+  echo ""
+  echo -e "${GREEN}Successfully compiled lab.${NOCOLOR}"
+  echo -e "${YELLOW}NOTE: Make sure you close all terminals after this message.${NOCOLOR}"
+  
+  source $HOME_DIR"/.bashrc"
+else
+  echo -e "${RED}FAILURE: Could not setup lab base code.${NOCOLOR}"
+  echo "Log output written to: "$LOG_FILE
+fi
+##################################################
