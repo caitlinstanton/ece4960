f7a75fc831f9c2e101c3bbf9325c535a50693ed0
Caitlin Stanton <caitlinstanton44@gmail.com>
2020-11-06 21:01:22 -0500

    lab9

--- lab7/scanning/settings.py
+++ lab7/scanning/settings.py
@@ -1,5 +1,5 @@
 Settings = {"adapter": "hci0",  # Not used in Windows and macOS
             "OutputRawData": False,
             "pingLoop": True,
-            "cached": "0EEEB402-2AC5-4904-BAFA-925974A4CBA1"
+            "cached": None
             }
